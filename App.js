45fba9e0777ac6b69a03863750cad877f3785c3a
SamirDhakal <68449594+SamirDhakal@users.noreply.github.com>
2022-03-23 20:59:22 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Main from './screens/main';
+import React from 'react';
+
+export default function App() {
+  return (
+    <Main/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
